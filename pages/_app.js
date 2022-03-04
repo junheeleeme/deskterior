5ef30eac65d843c71e64681b4c600da62e54c6f7
macjjuni <macjjuni@gmail.com>
2022-03-05 00:30:01 +0900

    fixed get menu fuc

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,18 @@ import { MenuContext } from '../context/MenuContext'
 import { ThumbContext } from '../context/ThumbContext'
 import { useEffect, useState } from 'react'
 
-function MyApp({ Component, pageProps, menu }) {
+const menu = [
+  { 'name' : '데스크탑', 'slug': 'desktop', 'categoryId': 65, 'description': '데스크탑 - DESKTOP', '__typename': 'Category'},
+  { 'name': '모니터', 'slug': 'monitor', 'categoryId': 66, 'description': '모니터 - MONITOR', '__typename': 'Category'},
+  { 'name' : '노트북', 'slug': 'notebook', 'categoryId': 67, 'description': '노트북 - NoteBook', '__typename': 'Category'},
+  {'name': '모니터암', 'slug': 'monitorarm', 'categoryId': 68, 'description': '모니터암 - MONITOR ARM', '__typename': 'Category'},
+  {'name': '키보드/마우스', 'slug': 'keyboardmouse', 'categoryId': 69, 'description': '키보드/마우스 - Keyboard/Mouse', '__typename': 'Category'},
+  {'name': '주변기기', 'slug': 'peripheral', 'categoryId': 70, 'description': '주변기기 - peripherals', '__typename': 'Category'},
+  {'name': 'PC부품', 'slug': 'pcparts', 'categoryId': 191, 'description': 'PC부품 - PC Parts', '__typename': 'Category'}
+]
+
+function MyApp({ Component, pageProps }) {
+
 
   const [thumb, setThumb] = useState();
   const _menu = menu.sort((a,b)=> a.categoryId-b.categoryId);
@@ -31,10 +42,5 @@ function MyApp({ Component, pageProps, menu }) {
   )
 }
 
-
-MyApp.getInitialProps = async () => {
-  const menu = await getMenu();
-  return { menu }
-}
 
 export default MyApp
