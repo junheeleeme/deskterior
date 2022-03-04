215781e8629e449e2f410f324a92244610be2f22
macjjuni <macjjuni@gmail.com>
2022-03-04 23:07:23 +0900

    fixed _app.js

--- apollo/apollo.js
+++ apollo/apollo.js
@@ -3,7 +3,6 @@ import ApolloClient, { InMemoryCache } from 'apollo-boost';
 const _host = process.env.NEXT_PUBLIC_URL;
 const host = process.env.URL;
 
-
 export const client = new ApolloClient({
     uri : `http://${host || _host }/juni`,
     cache: new InMemoryCache(),
